145c90efc68b3864801a65325d1c4bafdc3ca3fa
Owaish <owais.uaarav9@gmail.com>
2023-12-10 16:54:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ checkpoint = 'exp1'
 aug = True
 BERT_PATH = "bert-base-uncased"
 MODEL_PATH = "./model.pth"
-TRAINING_FILE = "/home/owaishs/temp2/Scripts/git-workspace/NLP/QandA/inputs/train.csv"
+TRAINING_FILE = "./inputs/train.csv"
 SPLIT = 0.75
 return_dict = False
 #TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
